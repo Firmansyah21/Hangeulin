66b2f55646d82253b68f8ba144f0ca750637e00f
syahfirman21 <syahfirman2106@gmail.com>
2023-12-05 22:37:35 +0700

    firt

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        kuning: "#F4C237",
+        p: "##1A1A1A",
+        button: "#BC0E19",
+        biru: "#045285",
+        pink: "#FEF7E5",
+        footer: "#FAE3A4",
+      },
+    },
+  },
+  plugins: [],
+};
